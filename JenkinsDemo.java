1dbc1a0092af08e6f901d9f585bf87b16f055b3c
deepshock618 <adex618@gmail.com>
2022-08-19 21:37:53 +0530

    updated

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,6 +1,6 @@
 public class JenkinsDemo
 {
-    public static void public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("yo");
     }
 }
